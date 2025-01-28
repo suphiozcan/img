b43db8175f708ef2cae2e8290fc2a5e2896af544
suphiozcan <suphiozcan2@gmail.com>
2025-01-28 11:53:55 +0200

    Update customside.js

--- customside.js
+++ customside.js
@@ -1,1 +1,44 @@
-test
+
+
+
+
+
+let siteLanguage = 'tr';
+
+const sidebarBig = document.querySelector('.sidebar-big');
+
+const newDiv = document.createElement('div');
+newDiv.classList.add('sidebar__link', 'custom_side');
+
+const link = document.createElement('a');
+link.classList.add('sidebar__link', 'sidebar__link--casino', 'custom_a');
+link.href = '#';
+link.style.height = '46';
+
+
+if (siteLanguage === 'en') {
+  link.style.background = 'url("https://betesbet.live/640x156-en.png") no-repeat center';
+} else {
+  link.style.background = 'url("https://betesbet.live/640x156-EMPTY.png") no-repeat center';
+}
+link.style.backgroundSize = 'cover';
+
+newDiv.appendChild(link);
+
+if (sidebarBig.children.length >= 1) {
+  sidebarBig.insertBefore(newDiv, sidebarBig.children[1]);
+} else {
+  sidebarBig.appendChild(newDiv);
+}
+
+function updateLanguage(newLanguage) {
+  siteLanguage = newLanguage;
+  if (siteLanguage === 'en') {
+    link.style.background = 'url("https://betesbet.live/640x156-en.png") no-repeat center';
+  } else {
+    link.style.background = 'url("https://betesbet.live/640x156-EMPTY.png") no-repeat center';
+  }
+  link.style.backgroundSize = 'cover';
+}
+
+updateLanguage('en');
